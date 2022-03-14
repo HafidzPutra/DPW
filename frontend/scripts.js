872e05d45636770ed472a3fc903b8bad9a727bb5
HafidzPutra <hafidzputrazainal230103@gmail.com>
2022-03-14 17:25:34 +0700

    first commit

---
+++ frontend/scripts.js
@@ -0,0 +1,7 @@
+const h1 = document.getElementById("hai")
+h1.style.color = "yellow";
+h1.style.backgroundColor = "blue";
+
+const h2 = document.getElementsByTagName("p");
+h2[0].style.color = "green";
+h2[0].style.backgroundColor = "red";
